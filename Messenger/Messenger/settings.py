bc79cbef1db87d0b3956bf5702ed3320df93c8e5
admin <nogerov2000@bk.ru>
2024-07-13 13:06:17 +0300

    New app

--- Messenger/Messenger/settings.py
+++ Messenger/Messenger/settings.py
@@ -32,16 +32,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'daphne',
+	
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+		
 	'chat',
-	'daphne',
-	
+	'channels',
+	'rest_framework',
+	'corsheaders',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Messenger.urls'
@@ -71,6 +78,7 @@ TEMPLATES = [
     },
 ]
 
+ASGI_APPLICATION = 'Messenger.asgi.application'
 WSGI_APPLICATION = 'Messenger.wsgi.application'
 
 
@@ -120,9 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+# 	os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Messenger/chat/routing.py
+++ Messenger/chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path(r'ws/chat/', consumers.ChatConsumers.as_asgi()),
+	re_path(r'ws/chat/', consumers.ChatConsumer.as_asgi()),
 ]
